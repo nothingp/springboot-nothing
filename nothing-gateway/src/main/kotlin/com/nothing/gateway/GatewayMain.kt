9030245c8ee38ddfbd17bc6ab7bcbe900826570b
jundy.chow <zhoujundi711@163.com>
2017-11-15 11:26:14 +0800

    init 提交api网关代码

---
+++ nothing-gateway/src/main/kotlin/com/nothing/gateway/GatewayMain.kt
@@ -0,0 +1,31 @@
+package com.nothing.gateway
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableOAuth2Sso
+class GatewayMain : WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.csrf().disable()
+    }
+
+    companion object {
+        @Throws(Exception::class)
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(GatewayMain::class.java, *args)
+        }
+    }
+
+
+}
