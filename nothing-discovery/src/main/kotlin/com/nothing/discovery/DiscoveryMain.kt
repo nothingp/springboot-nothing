b16cb8cd30817c4c7bd6fba9dc6b8d435c8d7c70
jundy.chow <zhoujundi711@163.com>
2018-01-03 11:54:20 +0800

    init 提交config和discovery

---
+++ nothing-discovery/src/main/kotlin/com/nothing/discovery/DiscoveryMain.kt
@@ -0,0 +1,20 @@
+package com.nothing.discovery
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+class DiscoveryMain{
+    companion object {
+        @Throws(Exception::class)
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(DiscoveryMain::class.java, *args)
+        }
+    }
+
+
+}
