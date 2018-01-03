1053cc49ed30075c0b7c3aaf65df5ceb74b199c4
jundy.chow <zhoujundi711@163.com>
2018-01-03 11:35:41 +0800

    init 调整gradle的配置

--- nothing-ucenter/src/main/kotlin/com/nothing/ucenter/repository/RoleRepository.kt
+++ nothing-ucenter/src/main/kotlin/com/nothing/ucenter/repository/RoleRepository.kt
@@ -3,7 +3,9 @@ package com.nothing.ucenter.repository
 import com.nothing.ucenter.model.Role
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.stereotype.Repository
 
+@Repository
 interface RoleRepository : JpaRepository<Role, Long>, JpaSpecificationExecutor<Role> {
 
 }
--- nothing-ucenter/src/main/kotlin/com/nothing/ucenter/repository/UserRepository.kt
+++ nothing-ucenter/src/main/kotlin/com/nothing/ucenter/repository/UserRepository.kt
@@ -4,7 +4,9 @@ package com.nothing.ucenter.repository
 import com.nothing.ucenter.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository: JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
     fun findFirstByEmail(email: String): User
